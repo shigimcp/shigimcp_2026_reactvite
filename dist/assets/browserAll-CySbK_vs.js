3af6977996ef056c5cb202162b2df1265f72a639
shigimcp <3861143+shigimcp@users.noreply.github.com>
2026-08-16 14:32:17 -0400

    Xstage: fixed <hr> tag for Safari

---
+++ dist/assets/browserAll-CySbK_vs.js
@@ -0,0 +1,1 @@
+import{e as i,A as e,C as a,c as s,E as t,F as n,D as d}from"./index-B8Q0iSzo.js";import"./webworkerAll-B20_jrdx.js";i.add(e);i.mixin(a,s);i.add(t);i.mixin(a,n);i.add(d);
--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
 		},
 
 		// base: "/",
-		base: "/shigimcp_2026_reactvite/",
+		// base: "/shigimcp_2026_reactvite/",
 		// base: "https://shigimcp.github.io/shigimcp_2026_reactvite/",
-		// base: "/Xstage/shigimcp_2026_reactvite/",
+		base: "/Xstage/shigimcp_2026_reactvite/",
 		// base: "https://www.shigimcp.com/Xstage/shigimcp_2026_reactvite/",
 		// base: "./",
 		// base: "",
